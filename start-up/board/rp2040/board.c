b1829973a957618a601ceb3626c2fa976a62cc6f
Aananth C N <c.n.aananth@gmail.com>
2022-09-05 21:26:16 +0530

    Standardized brd_ as the prefix for board or SoC specific.
    NammaAUTOSAR will use brd_ prefix for SoC specific implementations
    called by MCAL layer AUTOSAR APIs.

--- start-up/board/rp2040/board.c
+++ start-up/board/rp2040/board.c
@@ -26,7 +26,7 @@
 
 
 /* Functions */
-int uc_uart_init(u32 base) {
+int brd_uart_init(u32 base) {
         u32 baud_divint, baud_divfrac;
         u32 baud_rate_div;
 
@@ -93,7 +93,7 @@ int console_fputs(const char *s) {
 
 
 /* Microcontroller Subsystem Reset */
-int uc_ss_reset(void) {
+int brd_ss_reset(void) {
         RESET_CTRL = ~(
                 SS_BIT_TIMER |
                 SS_BIT_PLL_SYS |
@@ -110,7 +110,7 @@ int uc_ss_reset(void) {
 
 
 
-int uc_osc_init(void) {
+int brd_osc_init(void) {
         XOSC_CTRL    = XOSC_FREQ_RANGE_1_15MHz;
         XOSC_STARTUP = XOSC_DELAY((((XOSC_MHz * MHz) / CLOCK_SEC2MSEC) + 128) / 256);
         XOSC_CTRL   |= XOSC_ENABLE;
@@ -120,7 +120,7 @@ int uc_osc_init(void) {
 
 
 
-int uc_config_clock(u32 reg_base, u8 clksrc, u8 auxsrc) {
+int brd_config_clock(u32 reg_base, u8 clksrc, u8 auxsrc) {
         /* First reset the Clock src, else system may hang */
         *(volatile u32*)(reg_base + CLK_CTRL) &= ~(0x3);
         while(!((*(volatile u32*)(reg_base + CLK_SELECTED)) & (1)));
@@ -136,26 +136,26 @@ int uc_config_clock(u32 reg_base, u8 clksrc, u8 auxsrc) {
 }
 
 
-int uc_clock_init(void) {
+int brd_clock_init(void) {
         u32 clksrc, auxsrc;
 
         /* CLK_REF: SRC = XOSC = 12 MHz, No AUXSRC */
-        uc_config_clock(CLK_REF_BASE, 2, 0);
+        brd_config_clock(CLK_REF_BASE, 2, 0);
 
         /* CLK_SYS: SRC = SYS_AUX, AUXSRC = PLL_SYS = 125 MHz */
-        uc_config_clock(CLK_SYS_BASE, 1, 0);
+        brd_config_clock(CLK_SYS_BASE, 1, 0);
 
         /* CLK_PERI: AUXSRC = CLK_SYS = 125 MHz */
         CLK_PERI_CTRL = PERI_ENABLE | PERI_AUXSRC(0);
 
         /* CLK_USB: SRC = none, AUXSRC = PLL_USB = 48 MHz */
-        uc_config_clock(CLK_USB_BASE, 0, 0);
+        brd_config_clock(CLK_USB_BASE, 0, 0);
 
         /* CLK_ADC: SRC = none, AUXSRC = PLL_USB = 48 MHz */
-        uc_config_clock(CLK_ADC_BASE, 0, 0);
+        brd_config_clock(CLK_ADC_BASE, 0, 0);
 
         /* CLK_RTC: SRC = none, AUXSRC = PLL_USB = 48 MHz */
-        uc_config_clock(CLK_RTC_BASE, 0, 0);
+        brd_config_clock(CLK_RTC_BASE, 0, 0);
 
         /* CLK_GPOUTx: use 12 MHz crystal clock as clock source */
         /* Not sure if this initialization is needed for normal GPIO usage */
@@ -177,14 +177,14 @@ int uc_clock_init(void) {
     SYS PLL: 12 / 1    =   12MHz * 125 = 1500 MHz / 6 / 2  = 125 MHz
     USB PLL: 12 / 1    =   12MHz * 40  =  480 MHz / 5 / 2  =  48 MHz
 /*/
-int uc_pll_init(u32 base, u32 vco_freq_mhz, u8 post_div1, u8 post_div2) {
+int brd_pll_init(u32 base, u32 vco_freq_mhz, u8 post_div1, u8 post_div2) {
         u32 vco_freq = (vco_freq_mhz * MHz); 
         u32 refdiv = 1;
         u32 fbdiv, ref_mhz;
 
         /* let us switch to ROSC clock before PLL power down */
-        uc_config_clock(CLK_SYS_BASE, 0, 2);
-        uc_config_clock(CLK_REF_BASE, 0, 0);
+        brd_config_clock(CLK_SYS_BASE, 0, 2);
+        brd_config_clock(CLK_REF_BASE, 0, 0);
 
         /* power down PLL to configure it correctly */
         *((volatile u32*)(base+PLL_PWR_OFFSET)) = (1 << 5) | (1 << 3) | (1 << 2) | (1 << 0);
@@ -254,7 +254,7 @@ int brd_sys_enable_interrupts() {
 
 
 int brd_console_init(void) {
-        uc_uart_init(UART0_BASE);
+        brd_uart_init(UART0_BASE);
         pr_log_init();
 
         return 0;
@@ -262,11 +262,11 @@ int brd_console_init(void) {
 
 
 void Mcu_Init(const Mcu_ConfigType* ConfigPtr) {
-        uc_ss_reset();
-        uc_osc_init();
-        uc_pll_init(PLL_SYS_BASE, 1500, 6, 2); /*SYS_VCO: min = 5 MHz, max = 1600 MHz */
-        uc_pll_init(PLL_USB_BASE, 480, 5, 2);
-        uc_clock_init();
+        brd_ss_reset();
+        brd_osc_init();
+        brd_pll_init(PLL_SYS_BASE, 1500, 6, 2); /*SYS_VCO: min = 5 MHz, max = 1600 MHz */
+        brd_pll_init(PLL_USB_BASE, 480, 5, 2);
+        brd_clock_init();
         brd_setup_systimer();
         brd_console_init();
         brd_sys_enable_interrupts();
